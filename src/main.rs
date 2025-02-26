553d2a705ecbec2eb596976256c8c27f957e89bf
momowind-su <evgene.golo@gmail.com>
2025-02-26 14:54:34 +0200

    Step 2 fixed loop

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,13 @@ use std::io::{self, Write};
 
 fn main() {
     // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-   'infinite: loop {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
         // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        print!("{}: command not found", input.trim());
+        println!("{}: command not found", input.trim());
     }
 }
