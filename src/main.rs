aaa61a883c858a6899e609bc721188bc32a9b799
momowind-su <evgene.golo@gmail.com>
2025-02-26 17:19:25 +0200

    Refactored all the comands recognition to enum parsue and a match

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+mod commands;
+
+use std::{cmp, io::{self, Write}};
+use commands::Commands;
+
+
 
 fn main() {
-    // Uncomment this block to pass the first stage
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -12,9 +17,28 @@ fn main() {
        
 
         stdin.read_line(&mut input).unwrap();
-        if input.trim() == "exit 0" {
-            break;
+
+        let parts: Vec<String> = input.split_whitespace().map(String::from).collect();        
+        
+
+        let command: Commands = parts
+                                .get(0)
+                                .map(|cmd| cmd.parse().unwrap_or(Commands::Unknown(cmd.clone())))
+                                .unwrap_or(Commands::Unknown("".to_string()));
+        
+        let args= &parts[1..];
+
+        match command {
+            Commands::Exit0 => {
+                break;
+            }
+
+            Commands::Echo => {
+                println!("{}",args.join(" "));
+            }
+            Commands::Unknown(cmd) => {
+                println!("{}: command not found",cmd);
+            }
         }
-        println!("{}: command not found", input.trim());
     }
 }
