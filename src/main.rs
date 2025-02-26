7172110c8503e4e33afa29b8e63bff6551ca0c1e
momowind-su <evgene.golo@gmail.com>
2025-02-26 14:47:32 +0200

    Step 2 first atempt

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    print!("{}: command not found", input.trim());
+    loop {
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        print!("{}: command not found", input.trim());
+    }
 }
